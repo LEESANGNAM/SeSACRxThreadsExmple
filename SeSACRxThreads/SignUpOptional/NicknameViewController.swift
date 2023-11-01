7032b6197cebc6ce3f2040dbac5b058fbf5a8709
LEESANGNAM <fgh0116@naver.com>
2023-11-02 00:27:27 +0900

    11.1 RxSwift

--- SeSACRxThreads/SignUpOptional/NicknameViewController.swift
+++ SeSACRxThreads/SignUpOptional/NicknameViewController.swift
@@ -7,21 +7,74 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class NicknameViewController: UIViewController {
    
     let nicknameTextField = SignTextField(placeholderText: "닉네임을 입력해주세요")
     let nextButton = PointButton(title: "다음")
-    
+    let nicknameText = BehaviorSubject(value: "")
+    let baseColor = BehaviorSubject(value: UIColor.systemOrange)
+    let nickNameVaild = BehaviorSubject(value: false)
+    let disposebag = DisposeBag()
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = Color.white
         
         configureLayout()
-       
+        bind()
         nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
 
+    }
+    
+    func bind(){
+        // 1. 텍스트필드 값을 텍스트로 넘겨야함
+        nicknameTextField.rx.text.orEmpty
+            .subscribe(with: self) { owner, text in
+                let removeSpaceText = text.trimmingCharacters(in: .whitespacesAndNewlines)
+                owner.nicknameText.onNext(removeSpaceText) // 2. 텍스트필드값을 방출 ->
+            }
+        // 3. 여기서 방출받은값 받아서
+        // 텍스트를 검사하고 bool 값으로 변경해서
+        nicknameText.map { self.nicknameVaild(text: $0) }
+            .subscribe(with: self) { owner, value in
+                let color = value ? UIColor.systemGreen : UIColor.systemOrange
+                owner.baseColor.onNext(color) // 4. 컬러를 방출 ->
+                owner.nickNameVaild.onNext(value) //4. 조건검사값도 방출 ->
+            }.disposed(by: disposebag)
+        
+        
+        // 텍스트값이 바뀌면 텍스트필드에 보여주고
+        nicknameText
+            .bind(to: nicknameTextField.rx.text)
+            .disposed(by: disposebag)
+        // 조건을 검사해서 조건값을 버튼에 연결하고(통과못하면 클릭도 false)
+        nickNameVaild
+            .bind(to: nextButton.rx.isEnabled)
+            .disposed(by: disposebag)
+        // 컬러는 버튼의 배경과 연결하고
+        baseColor
+            .bind(to: nextButton.rx.backgroundColor)
+            .disposed(by: disposebag)
+        
+    }
+    
+    func nicknameVaild(text: String) -> Bool{
+        if text.isEmpty {
+            print("빈칸")
+            return false
+        } else if text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty{
+            print("공백만 있음")
+            return false
+        } else if text.count < 3 || text.count > 6{
+            print("닉네임을 3~6글자로 적어주세요")
+            return false
+        } else {
+            print("통과~")
+            return true
+        }
     }
     
     @objc func nextButtonClicked() {
--- SeSACRxThreads/SignUpOptional/PhoneViewController.swift
+++ SeSACRxThreads/SignUpOptional/PhoneViewController.swift
@@ -4,37 +4,75 @@
 //
 //  Created by jack on 2023/10/30.
 //
- 
+
 import UIKit
 import SnapKit
 import RxSwift
 import RxCocoa
 
 class PhoneViewController: UIViewController {
-   
+    
     let phoneTextField = SignTextField(placeholderText: "연락처를 입력해주세요")
     let nextButton = PointButton(title: "다음")
-    let phone = BehaviorSubject(value: "010")
-    
+    let phone = PublishSubject<String>()
+    let baseColor = BehaviorSubject(value: UIColor.systemRed)
+    let buttonEnabled = BehaviorSubject(value: false)
+    let disposebag = DisposeBag()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = Color.white
         
         configureLayout()
-        
+        bind()
         nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+    }
+    
+    func bind(){
+        
+        phone
+            .bind(to: phoneTextField.rx.text)
+            .disposed(by: disposebag)
+        
+        phoneTextField.rx.text.orEmpty
+            .subscribe(with: self) { owner, text in
+                let resultText = text.formated(by: "###-####-####")
+                owner.phone.onNext(resultText)
+            }.disposed(by: disposebag)
+        
+        
+        phone.map { $0.count > 12 }
+            .subscribe(with: self) { owner, value in
+                let color = value ? UIColor.systemBlue : UIColor.systemRed
+                owner.baseColor.onNext(color)
+//                owner.nextButton.isEnabled = value
+                owner.buttonEnabled.onNext(value)
+            }.disposed(by: disposebag)
+        
+        buttonEnabled
+            .bind(to: nextButton.rx.isEnabled)
+            .disposed(by: disposebag)
+        
+        baseColor
+            .bind(to: nextButton.rx.backgroundColor, phoneTextField.rx.tintColor)
+            .disposed(by: disposebag)
+        
+        baseColor
+            .map { $0.cgColor }
+            .bind(to: phoneTextField.layer.rx.borderColor)
+            .disposed(by: disposebag)
+        
     }
     
     @objc func nextButtonClicked() {
         navigationController?.pushViewController(NicknameViewController(), animated: true)
     }
-
+    
     
     func configureLayout() {
         view.addSubview(phoneTextField)
         view.addSubview(nextButton)
-         
+        
         phoneTextField.snp.makeConstraints { make in
             make.height.equalTo(50)
             make.top.equalTo(view.safeAreaLayoutGuide).offset(200)
@@ -47,5 +85,5 @@ class PhoneViewController: UIViewController {
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
     }
-
+    
 }
